d3a537167357526fd555ac99a03f9231d1b2a56f
Adam Riddell <riddelladam42@gmail.com>
2015-10-10 12:43:32 +1100

    Implement multi-stage capability
        app/app.js:
            - Add a stage list array to the stage controller.
            - Move part manipulation and calculation methods into
              newStage function, which acts as a "factory" for stage
              objects which are then added the stage list.
        app/index.html:
            - Add buttons for stage addition and removal.
            - Adapt the template to work with the new controller
              structure.

--- app/app.js
+++ app/app.js
@@ -5,11 +5,8 @@ var deevee = angular.module('deeVee', ['ngRoute']);
 deevee.controller('stageCtrl', ['$scope', '$http', function($scope, $http) {
     $http.get('parts/parts.json').success(function(data) {
       $scope.parts = data;
-      $scope.selected = data[0];
     });
-    $scope.result = '';
     $scope.gravity = 9.81;
-    $scope.partsList = []
     $scope.checkProperty = function(part, property) {
         if (typeof part[property] === 'number') {
             return part[property];
@@ -17,52 +14,67 @@ deevee.controller('stageCtrl', ['$scope', '$http', function($scope, $http) {
             return 0;
         }
     }
-    $scope.removePart = function(partid) {
-        if (partid > -1 && typeof $scope.partsList[partid] !== 'undefined') {
-           $scope.partsList.splice(partid, 1);
-        }
-        $scope.result = $scope.calculate();
+    $scope.stageList = [];
+    $scope.addStage = function() {
+        $scope.stageList.push($scope.newStage());
     }
-    $scope.addPart = function(part) {
-        var newpart = {'name' : part.name,
-                       'wetmass' : part.wetmass,
-                       'drymass' : part.drymass};
-        var properties = ['impulse', 'thrust'];
-
-        for (var i = 0; i < properties.length; i++) {
-            newpart[properties[i]] = $scope.checkProperty(part, properties[i]);
-        }
-        $scope.partsList.push(newpart);
-        $scope.selected = $scope.parts[0];
-        $scope.result = $scope.calculate();
+    $scope.removeStage = function(stageIndex) {
+        $scope.stageList.splice(stageIndex, 1);
     }
-    $scope.calculate = function() {
-        var sumdrymass, sumwetmass, sumthrust, sumfuelused, totalimpulse, dv,
-            thrustratio;
-        sumdrymass = sumwetmass = sumthrust = sumfuelused = totalimpulse = dv
-            = thrustratio = 0;
-
-        for (var i = 0; i < $scope.partsList.length; i++) {
-            sumdrymass += $scope.partsList[i].drymass;
-            sumwetmass += $scope.partsList[i].wetmass;
-            if ($scope.partsList[i].impulse !== 0) {
-                sumthrust += $scope.partsList[i].thrust;
-                sumfuelused += $scope.partsList[i].thrust
-                               / $scope.partsList[i].impulse;
+    $scope.newStage = function() {
+        var stage = {};
+        stage.result = '';
+        stage.partsList = []
+        stage.selected = $scope.parts[0];
+        stage.removePart = function(partid) {
+            if (partid > -1 && typeof stage.partsList[partid] !== 'undefined') {
+               stage.partsList.splice(partid, 1);
             }
+            stage.result = stage.calculate();
         }
-        if (sumfuelused !== 0) {
-            totalimpulse = sumthrust / sumfuelused;
+        stage.addPart = function(part) {
+            var newpart = {'name' : part.name,
+                           'wetmass' : part.wetmass,
+                           'drymass' : part.drymass};
+            var properties = ['impulse', 'thrust'];
+
+            for (var i = 0; i < properties.length; i++) {
+                newpart[properties[i]] = $scope.checkProperty(part, properties[i]);
+            }
+            stage.partsList.push(newpart);
+            stage.selected = stage.defaultSelected;
+            stage.selected = $scope.parts[0];
+            stage.result = stage.calculate();
         }
-        if (sumdrymass !== 0) {
-            dv = Math.log(sumwetmass / sumdrymass)
-                 * $scope.gravity * totalimpulse;
+        stage.calculate = function() {
+            var sumdrymass, sumwetmass, sumthrust, sumfuelused, totalimpulse, dv,
+                thrustratio;
+            sumdrymass = sumwetmass = sumthrust = sumfuelused = totalimpulse = dv
+                = thrustratio = 0;
+
+            for (var i = 0; i < stage.partsList.length; i++) {
+                sumdrymass += stage.partsList[i].drymass;
+                sumwetmass += stage.partsList[i].wetmass;
+                if (stage.partsList[i].impulse !== 0) {
+                    sumthrust += stage.partsList[i].thrust;
+                    sumfuelused += stage.partsList[i].thrust
+                                   / stage.partsList[i].impulse;
+                }
+            }
+            if (sumfuelused !== 0) {
+                totalimpulse = sumthrust / sumfuelused;
+            }
+            if (sumdrymass !== 0) {
+                dv = Math.log(sumwetmass / sumdrymass)
+                     * $scope.gravity * totalimpulse;
+            }
+            if (sumwetmass !== 0) {
+                thrustratio = sumthrust / (sumwetmass * $scope.gravity);
+            }
+            return {'impulse' : totalimpulse,
+                    'thrustratio' : thrustratio,
+                    'dv' : dv};
         }
-        if (sumwetmass !== 0) {
-            thrustratio = sumthrust / (sumwetmass * $scope.gravity);
-        }
-        return {'impulse' : totalimpulse,
-                'thrustratio' : thrustratio,
-                'dv' : dv};
+        return stage;
     }
 }]);
