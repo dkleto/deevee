b50e8d64003953a1a83a065778d1526d896eeada
Adam Riddell <riddelladam42@gmail.com>
2015-12-18 09:18:41 +1100

    Add new end-to-end tests
        - Test for adding a single part.
        - Test for adding multiple parts at once.
        - Test for part removal.

--- tests/e2e/scenarios.js
+++ tests/e2e/scenarios.js
@@ -7,8 +7,6 @@ describe('deeVee app', function() {
   });
   describe('Staging view', function() {
 
-
-
     it('should allow the user to add and remove stages', function() {
 
       var stageList = element.all(by.repeater('stage in stageList'));
@@ -23,19 +21,66 @@ describe('deeVee app', function() {
 
     });
   });
-  describe('Parts dialog view', function() {
+  describe('Part selection', function() {
 
-    it('should allow the user to select parts to add and remove', function() {
+    it('should allow the user to add a part', function() {
       var stageAdd = element(by.css('.stageAdd'));
-      var partAdd = element(by.css('.partAdd'));
+      var partAdd = element(by.css('.partAdd a'));
       var dialog = element(by.css('.dialog'));
       var partsList = element.all(by.repeater('part in stage.partsList'));
+      var catTitle = element.all(by.repeater('category in parts')).first();
+      var part = element.all(by.repeater('part in subcategory.parts')).first();
 
       stageAdd.click();
       expect(dialog.isPresent()).toBeFalsy;
       expect(partsList.count()).toBe(0);
       partAdd.click();
       expect(dialog.isPresent()).toBeTruthy;
+      catTitle.click();
+      part.click();
+      expect(partsList.count()).toBe(1);
+    });
+
+    it('should allow the user to add multiple parts', function() {
+      var stageAdd = element(by.css('.stageAdd'));
+      var partAdd = element(by.css('.partAdd a'));
+      var dialog = element(by.css('.dialog'));
+      var partsList = element.all(by.repeater('part in stage.partsList'));
+      var catTitle = element.all(by.repeater('category in parts')).first();
+      var part = element.all(by.repeater('part in subcategory.parts')).first();
+      var multiplier = element(by.css('.multiplier a'));
+
+      stageAdd.click();
+      expect(dialog.isPresent()).toBeFalsy;
+      expect(partsList.count()).toBe(0);
+      partAdd.click();
+      expect(dialog.isPresent()).toBeTruthy;
+      catTitle.click();
+      multiplier.click();
+      part.click();
+      expect(partsList.count()).toBe(2);
+    });
+
+    it('should allow the user to remove parts', function() {
+      var stageAdd = element(by.css('.stageAdd'));
+      var partAdd = element(by.css('.partAdd a'));
+      var dialog = element(by.css('.dialog'));
+      var partsList = element.all(by.repeater('part in stage.partsList'));
+      var catTitle = element.all(by.repeater('category in parts')).first();
+      var part = element.all(by.repeater('part in subcategory.parts')).first();
+      var multiplier = element(by.css('.multiplier a'));
+      var partRemove = element(by.css('.partRemove a'));
+
+      stageAdd.click();
+      expect(dialog.isPresent()).toBeFalsy;
+      expect(partsList.count()).toBe(0);
+      partAdd.click();
+      expect(dialog.isPresent()).toBeTruthy;
+      catTitle.click();
+      part.click();
+      expect(partsList.count()).toBe(1);
+      partRemove.click();
+      expect(partsList.count()).toBe(0);
 
     });
   });
